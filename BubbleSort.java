89a499e053fc030ac05b50de54b33c75c4a8dca0
akashdsingh006 <82928605+akashdsingh006@users.noreply.github.com>
2021-10-04 20:03:50 +0530

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,26 +1,34 @@
 import java.util.Scanner;
-class BubbleSort {
-	public static void main(String []args) {
-		int n, c, d, swap;
-		Scanner in = new Scanner(System.in);
-		System.out.println("Input number of integers to sort");
-		n = in.nextInt();
-		int array[] = new int[n];
-		System.out.println("Enter " + n + " integers");
-		for (c = 0; c < n; c++) 
-		      array[c] = in.nextInt();
-		for (c = 0; c < ( n - 1 ); c++) {
-			for (d = 0; d < n - c - 1; d++) {
-				if (array[d] > array[d+1]) 
-				/* For descending order use < */ {
-					swap       = array[d];
-					array[d]   = array[d+1];
-					array[d+1] = swap;
-				}
-			}
-		}
-		System.out.println("Sorted list of numbers");
-		for (c = 0; c < n; c++) 
-		      System.out.println(array[c]);
-	}
+
+class BubbleSortAlgo {
+  public static void main(String []args) {
+    int num, i, j, temp;
+    Scanner in = new Scanner(System.in);
+ 
+    System.out.println("Enter the number of integers to sort:");
+    num = in.nextInt();
+ 
+    int a[] = new int[num];
+ 
+    System.out.println("Enter " + num + " integers: ");
+ 
+    for (i = 0; i < num; i++) 
+      a[i] = in.nextInt();
+ 
+    for (i = 0; i < ( num - 1 ); i++) {
+      for (j = 0; j < num - i - 1; j++) {
+        if (a[j] > a[j+1]) 
+        {
+           temp = a[j];
+           a[j] = a[j+1];
+           a[j+1] = temp;
+        }
+      }
+    }
+ 
+    System.out.println("Sorted list of integers:");
+ 
+    for (i = 0; i < num; i++) 
+      System.out.println(a[i]);
+  }
 }
